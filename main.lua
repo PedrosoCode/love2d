cfed7b590c4da97e518a44fa1fa355daa7595e1e
Gabriel <Gabrielpedrofc@gmail.com>
2025-02-11 19:37:28 -0300

    movimento básico

--- main.lua
+++ main.lua
@@ -1,9 +1,29 @@
-
 function love.load()
-    Message = "Hello from LOVE"
-    local secret = 'This is a local string' -- local variable, apostrophes on the string.
-  end
+    player = {
+        x = 400,
+        y = 300,
+        speed = 300,
+        width = 40,
+        height = 60
+    }
+end
 
-  function love.draw()
-    love.graphics.print(Message, 200, 200)
-  end
+function love.update(dt)
+    -- Movimento
+    if love.keyboard.isDown('d') then
+        player.x = player.x + player.speed * dt
+    end
+    if love.keyboard.isDown('a') then
+        player.x = player.x - player.speed * dt
+    end
+    if love.keyboard.isDown('s') then
+        player.y = player.y + player.speed * dt
+    end
+    if love.keyboard.isDown('w') then
+        player.y = player.y - player.speed * dt
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
+end
